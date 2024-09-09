6036d66f8d4a2dca80f186e1d2c4a39d02dc0938
Ahmed Mahmoud <ahmedrosea20@gmail.com>
2024-09-09 06:42:17 +0300

    Update sdk

--- lib/shop_deal_app.dart
+++ lib/shop_deal_app.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:online_shop/core/global/theme/theme_data/dark_model_theme_data/dark_mode_theme_data.dart';
-import 'package:online_shop/core/global/theme/theme_data/light_mode_theme_data/light_mode_theme_data.dart';
-import 'package:online_shop/core/utils/app_router.dart';
-import 'package:online_shop/core/utils/service_locator.dart';
-import 'package:online_shop/features/account/view_model/address_cubit/address_cubit.dart';
-import 'package:online_shop/features/account/view_model/user_cubit/user_cubit.dart';
-import 'package:online_shop/features/authentication/view_model/login_cubit/login_cubit.dart';
-import 'package:online_shop/features/authentication/view_model/register_cubit/register_cubit.dart';
-import 'package:online_shop/features/cart/view_model/cart_cubit/cart_cubit.dart';
-import 'package:online_shop/features/checkout/view_model/checkout_cubit/checkout_cubit.dart';
-import 'package:online_shop/features/core/view_model/internet_cubit/internet_cubit.dart';
-import 'package:online_shop/features/core/view_model/app_cubit/app_cubit.dart';
-import 'package:online_shop/generated/l10n.dart';
+import 'package:shop_deal/core/global/theme/theme_data/dark_model_theme_data/dark_mode_theme_data.dart';
+import 'package:shop_deal/core/global/theme/theme_data/light_mode_theme_data/light_mode_theme_data.dart';
+import 'package:shop_deal/core/utils/app_router.dart';
+import 'package:shop_deal/core/utils/service_locator.dart';
+import 'package:shop_deal/features/account/view_model/address_cubit/address_cubit.dart';
+import 'package:shop_deal/features/account/view_model/user_cubit/user_cubit.dart';
+import 'package:shop_deal/features/authentication/view_model/login_cubit/login_cubit.dart';
+import 'package:shop_deal/features/authentication/view_model/register_cubit/register_cubit.dart';
+import 'package:shop_deal/features/cart/view_model/cart_cubit/cart_cubit.dart';
+import 'package:shop_deal/features/checkout/view_model/checkout_cubit/checkout_cubit.dart';
+import 'package:shop_deal/features/core/view_model/internet_cubit/internet_cubit.dart';
+import 'package:shop_deal/features/core/view_model/app_cubit/app_cubit.dart';
+import 'package:shop_deal/generated/l10n.dart';
 
 class OnlineShopApp extends StatelessWidget {
   const OnlineShopApp({super.key});
